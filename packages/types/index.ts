813ae5a0628e60e6cd4a4416f638076b66335bc5
Aaron Huggins <ahuggins@nethealthshops.com>
2021-02-02 15:18:12 -0600

    Improve snapshot metadata interface

--- packages/types/index.ts
+++ packages/types/index.ts
@@ -6,11 +6,6 @@
  * @module @jsvfs/types
  */
 
-/** The valid item types implemented by jsvfs. */
-export type LinkType = 'hardlink' | 'softlink'
-export type FolderType = 'folder'| 'root'
-export type ItemType = 'file' | FolderType | LinkType
-
 /** An adapter for the underlying persistent file storage.
  * 
  * Rules for implementing an adapter:
@@ -21,7 +16,7 @@ export type ItemType = 'file' | FolderType | LinkType
  */
 export interface Adapter {
   /** Snapshot of the underlying file system; an asynchronous iterable which returns an entry of path and data. */
-  snapshot (): AsyncGenerator<[string, 'folder' | Buffer]>
+  snapshot (): AsyncGenerator<[string, SnapshotEntry]>
   /** Create a file or write the contents of a file to persistent storage. */
   write (path: string, contents?: Buffer): Promise<void>
   /** Make a directory or directory tree in persistent storage. */
@@ -40,10 +35,43 @@ export interface Adapter {
   handle: string
 }
 
+/** Types the indicate the link behavior. */
+export type LinkType = 'hardlink' | 'softlink'
+/** Types that implement folder or "parent item" functionality. */
+export type FolderType = 'folder'| 'root'
+/** The valid item types implemented by jsvfs. */
+export type ItemType = 'file' | FolderType | LinkType
+/** Types of entries which may be encountered during a snapshot. */
+export type SnapshotEntry = SnapshotFileEntry | SnapshotFolderEntry | SnapshotLinkEntry
+
+/** Metadata about an event for the adapter journal. */
 export interface JournalEntry {
   id: string | number
   op: 'snapshot' | 'write' | 'mkdir' | 'link' | 'remove' | 'flush'
   level: 'info' | 'warn' | 'error' | 'crit'
   message: string
   [property: string]: any
+}
+
+/** Metadata about the file system captured by the snapshot. */
+interface SnapshotEntryBase {
+  /** The type of entry, such as 'file' or 'folder'. */
+  type: ItemType
+  contents?: any
+}
+
+export interface SnapshotFileEntry extends SnapshotEntryBase {
+  type: 'file'
+  /** The contents of the file as a buffer. */
+  contents: Buffer
+}
+
+export interface SnapshotFolderEntry extends SnapshotEntryBase {
+  type: 'folder'
+}
+
+export interface SnapshotLinkEntry extends SnapshotEntryBase {
+  type: LinkType
+  /** The target path of the link. */
+  contents: string
 }
