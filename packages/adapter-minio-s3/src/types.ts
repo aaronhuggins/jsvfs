625029c801c49fa3d672fc33fe466b75f43963d1
Aaron Huggins <ahuggins@nethealthshops.com>
2021-02-10 21:23:48 -0600

    Initial commit of s3 support

---
+++ packages/adapter-minio-s3/src/types.ts
@@ -0,0 +1,24 @@
+import type { ClientOptions } from 'minio'
+
+/** Options to construct an adapter instance. */
+export interface MinioS3AdapterOpts {
+  /** The options to connect to and access an Amazon S3 compatible storage. */
+  access: ClientOptions
+  /** The file globs to apply to `snapshot` and `flush` operations. */
+  include?: string[]
+  /**
+   * If no bucket name is provided, or bucket name is set to '/', then the storage account is considered root.
+   * Providing any other valid string will be considered a bucket name, making the bucket the root.
+   */
+  bucketName?: string
+  /** Enable flushing the file system before commiting; defaults to false, since flush is a destructive operation. */
+  flushEnabled?: boolean
+  /** Enable creating buckets if they do not exist; defaults to false. */
+  createIfNotExist?: boolean
+}
+
+/** The result of parsing a path. */
+export interface PathParseResult {
+  bucketName: string
+  blobName: string
+}
